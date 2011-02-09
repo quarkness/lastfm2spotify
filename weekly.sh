3ca36d929ec6a1e773644e2659a95e47a472c82d
Ivo van Doesburg <ivo@nrcdigitaal.nl>
2011-02-09 13:58:45 +0100

    automatiseren is de bom

--- weekly.sh
+++ weekly.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 curl http://ws.audioscrobbler.com/2.0/user/$1/weeklytrackchart.xml | xsltproc lastfm2txt.xsl - >weeklytrackchart.$1.txt
-
+php txt2spotify.php $1
+mate weeklytrackchart.$1.spotify
