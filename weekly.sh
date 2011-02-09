10c97cb3e70864e28839b0580eadf05de1209bbe
Ivo van Doesburg <ivo@nrcdigitaal.nl>
2011-02-09 20:27:36 +0100

    copies result to clipboard

--- weekly.sh
+++ weekly.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+echo "Getting weekly track chart from last.fm"
 curl http://ws.audioscrobbler.com/2.0/user/$1/weeklytrackchart.xml | xsltproc lastfm2txt.xsl - >weeklytrackchart.$1.txt
+echo "Querying Spotify API for track id's"
 php txt2spotify.php $1
-mate weeklytrackchart.$1.spotify
+cat weeklytrackchart.$1.spotify |pbcopy
+echo "Spotify playlist copied to clipboard. Enjoy"
