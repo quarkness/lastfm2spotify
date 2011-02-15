706d2e62334aae8d4ca40a1d645508f22d06a20f
Ivo van Doesburg <ivo@nrcdigitaal.nl>
2011-02-15 22:03:18 +0100

    clean up

--- kx-collect.sh
+++ kx-collect.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Getting playing data from kxradio"
 curl -s --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0b8) Gecko/20100101 Firefox/4.0b8"  http://kxredhot.nl/playing.php >>kx.redhot.txt
-cat kx.redhot.txt |sort |uniq >temp.txt
+cat kx.redhot.txt |grep -v kx |sed 's/^﻿*//g' |sort -u >temp.txt
 mv temp.txt kx.redhot.txt
